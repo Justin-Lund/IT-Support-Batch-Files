f0c5a3115995512501e2f65a5b1e3ef7e00f362a
Justin Lund <47057520+Justin-Lund@users.noreply.github.com>
2019-10-14 19:30:28 -0700

    Update and rename Remote Troubleshooting Tools 1.0.bat to Remote Troubleshooting Tools 1.2.bat

--- Remote Scripts/Remote Troubleshooting Tools 1.2.bat
+++ Remote Scripts/Remote Troubleshooting Tools 1.2.bat
@@ -1,21 +1,23 @@
 @echo off
-:: Remote Troubleshooting Tools 1.0
+:: Remote Troubleshooting Tools 1.2
 :: https://github.com/Justin-Lund/
 
 :Start
 cls
-title Remote Troubleshooting Tools 1.0
+title Remote Troubleshooting Tools 1.2
 
 
 echo 1. Force Group Policy Update
 echo 2. Clear Printer Cache
 echo 3. Get List of Programs on Computer
-echo 4. Exit Remote Troubleshooting Tools
+echo 4. Fix Failing Updates
+echo 5. Exit Remote Troubleshooting Tools
 echo.
 
-CHOICE /C 1234 /M "Enter your choice:"
+CHOICE /C 12345 /M "Enter your choice:"
 
-IF ERRORLEVEL 4 GOTO Exit
+IF ERRORLEVEL 5 GOTO Exit
+IF ERRORLEVEL 4 GOTO FixUpdates
 IF ERRORLEVEL 3 GOTO ListPrograms
 IF ERRORLEVEL 2 GOTO PrinterCache
 IF ERRORLEVEL 1 GOTO RemoteGPUpdate
@@ -31,7 +33,7 @@ echo.
 echo Running gpupdate /force on %Computer Name%
 echo.
 
-psExec 2>nul \\%Computer Name% gpupdate /force
+psExec -s -i -d \\VHOL1617411 cmd /c "start /min gpupdate /force"
 
 echo.
 pause
@@ -48,7 +50,9 @@ echo.
 
 psService 2>nul \\%Computer Name% stop spooler
 psExec 2>nul \\%Computer Name% cmd /c del /f /q %systemroot%\system32\spool\printers\*.*
+psExec 2>nul \\%Computer Name% reg DELETE "HKLM\SYSTEM\CurrentControlSet\Control\Print\Environments\Windows x64\Drivers\Version-3\Xerox Global Print Driver PCL6" /f /va
 psService 2>nul \\%Computer Name% start spooler
+
 
 echo.
 pause
@@ -67,6 +71,23 @@ echo The list of programs will be written to C:\Temp\InstallList.txt
 del C:\Temp\InstallList.txt 
 
 wmic /node:%Computer Name% /output:C:\Temp\InstallList.txt product get name,version
+
+echo.
+pause
+GOTO Start
+
+
+:FixUpdates
+cls
+title Fixing Failing Updates
+
+Set /p Computer Name=Enter Computer Name or I.P. Address:
+
+echo.
+echo Fixing updates on %Computer Name%
+echo.
+
+powershell.exe -Command "Invoke-Command -ScriptBlock {Stop-Service "wuauserv"; Stop-Service "CcmExec"; Remove-Item -Path "C:\Windows\SoftwareDistribution\*" -Recurse ;Start-Service "wuauserv"; Start-Service "CcmExec" } -ComputerName %Computer Name%"
 
 echo.
 pause
